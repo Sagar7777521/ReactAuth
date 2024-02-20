bc4baeb19719455bf0177fc67286fa739bb04ecd
Sagar <70952463+Sagar7777521@users.noreply.github.com>
2024-02-20 18:50:57 +0530

    Completed Signin Functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function Signup() {
+export default function Signin() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -13,7 +14,7 @@ export default function Signup() {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await fetch("/api/auth/signup", {
+      const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -21,12 +22,12 @@ export default function Signup() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      console.log(data);
       setLoading(false);
       if (data.success === false) {
         setError(true);
         return;
       }
+      navigate("/");
       console.log(data);
     } catch (error) {
       setLoading(false);
@@ -65,10 +66,16 @@ export default function Signup() {
         <button className="bg-red-700 uppercase text-white rounded-lg p-3 ml-10 hover:opacity-90 font-medium">
           {loading ? "Loading..." : "SIGN IN USING GOOGLE"}
         </button>
+        <div className="flex gap-2 ml-10">
+          <p>Dont have an account?</p>
+          <Link to="/signup">
+            <span className=" text-blue-600">Sign up</span>
+          </Link>
+        </div>
       </form>
       <div className="flex">
         <p className="text-red-700 mt-1 ml-20">
-          {error && "Username or email address already exist!"}
+          {error && "Email address is not registered or password is wrong!"}
         </p>
       </div>
     </div>
